45631bb1400aec003767aafdd6a0fde84c520cdc
ColtonGlasgow13 <coleglasgow13@gmail.com>
2019-12-15 23:36:54 -0500

    Dec 15 DynamicPlotter

--- src/dynamic_plotter.py
+++ src/dynamic_plotter.py
@@ -16,22 +16,30 @@ variables will be not included in a 'many' data structure thus having a sample s
 point will be removed and one new x and y data point added.
 
 """
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.figure import Figure
-import matplotlib.pyplot as plt
-from matplotlib.lines import Line2D
-import numpy
+from __future__ import unicode_literals
+import sys
+import os
+import random
+import matplotlib
+# Make sure that we are using QT5
 matplotlib.use('Qt5Agg')
 from PyQt5 import QtCore, QtWidgets
+from numpy import arange, sin, pi
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+
+progname = os.path.basename(sys.argv[0])
+progversion = "0.1"
 
 
 class DynamicPlotter(FigureCanvas):
 
-    def __init__(self, title, num_samples, parent=None, width=5, height=4, dpi=100):
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
         fig = Figure(figsize=(width, height), dpi=dpi)
         self.axes = fig.add_subplot(111)
 
-        self.compute_initial_figure()
+        self.add_line('origin', [0], [0])
 
         FigureCanvas.__init__(self, fig)
         self.setParent(parent)
@@ -41,27 +49,90 @@ class DynamicPlotter(FigureCanvas):
                                    QtWidgets.QSizePolicy.Expanding)
         FigureCanvas.updateGeometry(self)
 
-    def compute_initial_figure(self):
+    def add_line(self):
         pass
 
-    #Not sure where the format variable is used here
-    def add_line(self, name, x_data, y_data, format):
-        line = Line2D(x_data, y_data)
-        ax.add_line(line)
-        ax.set_xlim(min(x), max(x))
-        ax.set_ylim(min(y), max(y))
-        ax.plot()
-        lines[name] = line
+
+
+class MyDynamicMplCanvas(DynamicPlotter):
+
+    """A canvas that updates itself every second with a new plot."""
+
+    def __init__(self, *args, **kwargs):
+        DynamicPlotter.__init__(self, *args, **kwargs)
+        timer = QtCore.QTimer(self)
+        timer.timeout.connect(self.update_figure)
+        timer.start(1000)
+
+    def add_line(self, name, x_data, y_data, format=None):
+        lines[name] = plt.Line2D(x_data, y_data, format)
+
+    def update_figure(self):
+        x_data = [random.randint(0, 10)]
+        y_data = [random.randint(0, 10)]
+        self.axes.cla()
+        self.add_data('origin', x_data, y_data)
+        line = lines['origin']
+        self.axes.plot(line.get_xydata())
+        self.draw()
 
     def add_data(self, name, x_data, y_data):
         line = lines[name]
 
-        if len(line.get_xdata) < num_samples:
-            line.set_xdata(numpy.append(line.get_xdata(), x_data))
-            line.set_ydata(numpy.append(line.get_ydata(), y_data))
-        else:
-            # sets each data to the latest num_samples-1 points of data plus the new point
-            line.set_xdata(numpy.append(line.get_xdata()[len(line.get_xdata)-num_samples+1:], x_data))
-            line.set_ydata(numpy.append(line.get_ydata()[len(line.get_ydata)-num_samples+1:], y_data))
+        line.set_data(line.get_xdata() + x_data, line.get_ydata() + y_data)
 
         lines[name] = line
+
+
+
+class ApplicationWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        QtWidgets.QMainWindow.__init__(self)
+        self.setAttribute(QtCore.Qt.WA_DeleteOnClose)
+        self.setWindowTitle("application main window")
+
+        self.file_menu = QtWidgets.QMenu('&File', self)
+        self.file_menu.addAction('&Quit', self.fileQuit,
+                                 QtCore.Qt.CTRL + QtCore.Qt.Key_Q)
+        self.menuBar().addMenu(self.file_menu)
+
+        self.help_menu = QtWidgets.QMenu('&Help', self)
+        self.menuBar().addSeparator()
+        self.menuBar().addMenu(self.help_menu)
+
+        self.help_menu.addAction('&About', self.about)
+
+        self.main_widget = QtWidgets.QWidget(self)
+
+        l = QtWidgets.QVBoxLayout(self.main_widget)
+        #sc = MyStaticMplCanvas(self.main_widget, width=5, height=4, dpi=100)
+        dc = MyDynamicMplCanvas(self.main_widget, width=5, height=4, dpi=100)
+        #l.addWidget(sc)
+        l.addWidget(dc)
+
+        self.main_widget.setFocus()
+        self.setCentralWidget(self.main_widget)
+
+        self.statusBar().showMessage("All hail matplotlib!", 2000)
+
+    def fileQuit(self):
+        self.close()
+
+    def closeEvent(self, ce):
+        self.fileQuit()
+
+    def about(self):
+        QtWidgets.QMessageBox.about(self, "About",
+                                    """balls"""
+                                )
+
+
+lines = {}
+
+qApp = QtWidgets.QApplication(sys.argv)
+
+aw = ApplicationWindow()
+aw.setWindowTitle("%s" % progname)
+aw.show()
+sys.exit(qApp.exec_())
+#qApp.exec_()
